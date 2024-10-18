3c963231f70488b00da9ad892613ffbfea21c28f
s r <sr@beanz.zip>
2024-10-18 17:43:04 +0200

    added load balancer

--- Modules/Network/main.tf
+++ Modules/Network/main.tf
@@ -1,22 +1,7 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.0.1"
-    }
-  }
-}
-
 provider "azurerm" {
   features {  }
   subscription_id = "7c064ed9-c59f-4935-938b-f1a654d088a7"
 
-}
-
-resource "azurerm_network_security_group" "nsg" {
-  name                = "nsg-${var.base-name}-01"
-  location            = var.location
-  resource_group_name = var.rg-name
 }
 
 resource "azurerm_virtual_network" "vnet" {
@@ -33,7 +18,13 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes = ["10.0.1.0/24"]
 }
 
-resource "azurerm_subnet_network_security_group_association" "nsg-association" {
-  subnet_id = azurerm_subnet.subnet.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
+resource "azurerm_lb" "example" {
+  name                = "TestLoadBalancer"
+  location            = var.location
+  resource_group_name = var.rg-name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    private_ip_address = "10.0.0.69"
+  }
 }
