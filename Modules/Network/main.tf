965f92a1f292996fbd1ac5b14df06bd53220e8fb
s r <sr@beanz.zip>
2024-10-18 18:33:35 +0200

    added public ip resource for lb

--- Modules/Network/main.tf
+++ Modules/Network/main.tf
@@ -4,8 +4,15 @@ provider "azurerm" {
 
 }
 
+resource "azurerm_public_ip" "ip" {
+  name                = "lbip-${var.base-name}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-${var.base-name}-01"
+  name                = "vnet-${var.base-name}"
   location            = var.location
   resource_group_name = var.rg-name
   address_space       = ["10.0.0.0/16"]
@@ -18,13 +25,13 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes = ["10.0.1.0/24"]
 }
 
-resource "azurerm_lb" "example" {
-  name                = "TestLoadBalancer"
+resource "azurerm_lb" "loadbalancer" {
+  name                = "lb-${var.base-name}"
   location            = var.location
   resource_group_name = var.rg-name
 
   frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    private_ip_address = "10.0.0.69"
+    name                 = azurerm_public_ip.ip.name
+    public_ip_address_id = azurerm_public_ip.ip.id
   }
 }
